b1745a6e9853556c689a6ccc5f813c39950623df
daniel2dfla <daniel_2dfla@hotmail.com>
2024-03-21 20:29:53 -0300

    Added validations and started authentications

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     RestaurantsModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    AuthModule
   ],
   controllers: [],
   providers: [],
--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,11 +1,33 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsNumber, IsPhoneNumber, IsString } from "class-validator";
 import { Category } from "../schemas/restaurant.schema";
 
 export class CreateRestaurantDto {
+    @IsNotEmpty()
+    @IsString()
     readonly name: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly description: string;
+
+    @IsNotEmpty()
+    @IsEmail({} , { message: "please, enter correct email" })
     readonly email: string;
+
+    @IsNotEmpty()
+    @IsPhoneNumber('BR')
     readonly phone: number;
+
+    @IsNotEmpty()
+    @IsString()
     readonly address: string;
+
+    @IsNotEmpty()
+    @IsEnum(Category, { message: "please, enter correct category" })
     readonly category: Category;
-    readonly image?: object[];
+
+    @IsNumber()
+    readonly cep: number;
+
+    //readonly image?: object[];
 }
